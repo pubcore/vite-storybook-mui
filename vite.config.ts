d755811aa359196867aa197adfc18df563b895e5
jacklrs <github@rushsoft.de>
2021-11-25 09:52:36 +0100

    fix: declaration files are missing;

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { peerDependencies } from "./package.json";
+
+import typescript from "@rollup/plugin-typescript";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/components/index.ts"),
+      entry: "./src/components/index.ts",
       name: "@pubcore/vite-storybook-mui",
       fileName: (format) => `index.${format}.js`,
       formats: ["es"],
     },
     rollupOptions: {
       external: Object.keys(peerDependencies),
+      plugins: [
+        typescript({
+          declaration: true,
+          declarationDir: "./dist",
+          include: "./src/components/**/*",
+          exclude: "./src/**/*.stories.tsx",
+        }),
+      ],
     },
     sourcemap: true,
   },
