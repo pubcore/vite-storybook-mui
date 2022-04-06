eb940ad0d72cc16386b58017b6699fff1bd6ca60
Sven <s.fehler@pbsnetwork.eu>
2022-04-06 11:42:51 +0200

    refactor: rjsf to work with AMP

--- src/components/JsonSchemaForm/FieldTemplate.tsx
+++ src/components/JsonSchemaForm/FieldTemplate.tsx
@@ -1,18 +1,36 @@
 import { HelpOutline } from "@mui/icons-material";
-import { Box, IconButton, Typography } from "@mui/material";
+import {
+  Box,
+  DialogActions,
+  DialogContent,
+  IconButton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { FieldTemplateProps } from "@rjsf/core";
 import { useState } from "react";
-import Dialog from "../Dialog/Dialog";
+import { useTranslation } from "react-i18next";
+import { ActionButton } from "../Button";
+import { Dialog } from "../Dialog/Dialog";
+import { A } from "../Link";
 
 // see https://react-jsonschema-form.readthedocs.io/en/v1.8.1/advanced-customization/#field-template
 export function FieldTemplate(props: FieldTemplateProps) {
-  const { id, label, description, children, errors, help } = props;
+  const { t } = useTranslation();
 
-  const isRootField = id === "rjsf";
+  const { label, description, children, errors, uiSchema } = props;
 
-  console.log("Field template props:", props);
+  const uiField = uiSchema?.["ui:field"];
+  const pdfUri = uiSchema?.["ui:options"]?.helpUri;
+
+  console.log(`Props for field template '${uiField}':`, props);
 
   const [isHelpDialogOpen, setIsHelpDialogOpen] = useState(false);
+
+  const { breakpoints } = useTheme();
+
+  const isMobile = !useMediaQuery(breakpoints.up("sm"));
 
   return (
     <Box
@@ -22,18 +40,54 @@ export function FieldTemplate(props: FieldTemplateProps) {
         justifyContent: "space-between",
       }}
     >
-      {isHelpDialogOpen ? (
+      {typeof pdfUri === "string" && isHelpDialogOpen ? (
         <Dialog
           {...{
             open: isHelpDialogOpen,
-            title: "PDF Dialog",
             onClose: () => setIsHelpDialogOpen(false),
+            fullWidth: true,
+            PaperProps: {
+              sx: {
+                maxWidth: 800,
+              },
+            },
           }}
         >
-          <div>Test</div>
+          <DialogContent
+            sx={{
+              height: {
+                sm: 450,
+                md: 600,
+              },
+              overflow: "hidden",
+            }}
+          >
+            <object
+              data={pdfUri}
+              type="application/pdf"
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <p>
+                <A href={pdfUri} target="_blank">
+                  {t("open_help_document")}
+                </A>
+              </p>
+            </object>
+          </DialogContent>
+          <DialogActions>
+            <ActionButton
+              variant="contained"
+              onClick={() => setIsHelpDialogOpen(false)}
+            >
+              {t("close")}
+            </ActionButton>
+          </DialogActions>
         </Dialog>
       ) : null}
-      {label && !isRootField ? (
+      {label && uiField ? (
         <Box
           sx={{
             display: "flex",
@@ -50,25 +104,41 @@ export function FieldTemplate(props: FieldTemplateProps) {
           >
             <Typography>{label}</Typography>
           </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-around",
-              marginRight: 2,
-            }}
-          >
-            <IconButton onClick={() => setIsHelpDialogOpen(true)}>
-              <HelpOutline />
-            </IconButton>
-          </Box>
+          {typeof pdfUri === "string" ? (
+            !isMobile ? (
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-around",
+                  marginRight: 2,
+                }}
+              >
+                <IconButton onClick={() => setIsHelpDialogOpen(true)}>
+                  <HelpOutline />
+                </IconButton>
+              </Box>
+            ) : (
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-around",
+                  marginRight: 2,
+                }}
+              >
+                <A href={pdfUri}>
+                  <HelpOutline />
+                </A>
+              </Box>
+            )
+          ) : null}
         </Box>
       ) : null}
       <Box>
         {description}
         {children}
         {errors}
-        {help}
       </Box>
     </Box>
   );
