15711018e6f33c9302f03a4bed0296e26be51ece
Sven <s.fehler@pbsnetwork.eu>
2022-07-17 20:41:26 +0200

    fix: rest props passed through in actionbar

--- src/components/ActionBar/ActionBar.stories.tsx
+++ src/components/ActionBar/ActionBar.stories.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import { ActionButton } from "..";
 import ActionBar from "./";
 
@@ -11,3 +12,21 @@ export const Default = () => (
     <ActionButton>save</ActionButton>
   </ActionBar>
 );
+
+export const Alignments = () => {
+  const getCont = (txt: string) => (
+    <>
+      <Box sx={{ margin: 2 }}>{txt}</Box>
+      <ActionButton>button</ActionButton>
+    </>
+  );
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 8 }}>
+      <ActionBar>{getCont("Default")}</ActionBar>
+      <ActionBar sx={{ flexDirection: "row" }}>{getCont("Left")}</ActionBar>
+      <ActionBar sx={{ flexDirection: "row-reverse" }}>
+        {getCont("Right")}
+      </ActionBar>
+    </Box>
+  );
+};
--- src/components/ActionBar/index.tsx
+++ src/components/ActionBar/index.tsx
@@ -2,9 +2,13 @@ import { Toolbar, AppBar, AppBarProps } from "@mui/material";
 
 export type ActionBarProps = AppBarProps;
 
-export default function ActionBar({ children, elevation = 0 }: ActionBarProps) {
+export default function ActionBar({
+  children,
+  elevation = 0,
+  ...rest
+}: ActionBarProps) {
   return (
-    <AppBar position="static" color="transparent" {...{ elevation }}>
+    <AppBar position="static" color="transparent" {...{ elevation, ...rest }}>
       <Toolbar sx={{ justifyContent: "space-between" }}>{children}</Toolbar>
     </AppBar>
   );
