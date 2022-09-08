2df898cb6f475ad37d51770b698068128bba1a01
Sven <s.fehler@pbsnetwork.eu>
2022-09-08 10:00:11 +0200

    feat: support for multiple files in FileUpload

--- src/components/FileUpload/FileUpload.stories.tsx
+++ src/components/FileUpload/FileUpload.stories.tsx
@@ -3,13 +3,21 @@ import { FileUpload, FileUploadProps } from "../";
 export default {
   title: "Inputs/File upload",
   args: {
-    handleFile: () => new Promise((res) => setTimeout(() => res(), 2000)),
+    handleFile: ({ formData }) =>
+      new Promise((res) => {
+        console.log("Handle single file:", formData.get("file")!);
+        setTimeout(res, 2000);
+      }),
   } as Args,
 };
 
 type Args = FileUploadProps;
 
-export const Default = (args: Args) => <FileUpload {...{ ...args }} />;
+export const Default = (args: Args) => (
+  <FileUpload {...{ ...args }}>
+    Drag and drop or click to upload a file
+  </FileUpload>
+);
 
 export const OnlyImages = (args: Args) => (
   <FileUpload
@@ -23,5 +31,23 @@ export const OnlyImages = (args: Args) => (
         "image/tiff": [".tif", ".tiff"],
       },
     }}
-  />
+  >
+    Drag and drop or click to upload an image
+  </FileUpload>
+);
+
+export const MultipleFiles = (args: Args) => (
+  <FileUpload
+    {...{
+      ...args,
+      handleFile: undefined,
+      handleFiles: ({ files }) =>
+        new Promise((res) => {
+          console.log("Handle multiple files:", files);
+          setTimeout(res, 2000);
+        }),
+    }}
+  >
+    Drag and drop or click to upload files
+  </FileUpload>
 );
